4e5780391e6f2e66e2371c3e46e4792862994136
AshishSatyal <ashishsatyal4@gmail.com>
2024-06-30 23:26:09 +0545

    Add:HOC (Center Component)

---
+++ frontend/src/component/CenterComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CenterComponent = ({ children }) => {
+  return <div className='mx-auto max-w-[1200px]'>{children}</div>;
+};
+
+export default CenterComponent;
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CenterComponent from "../component/CenterComponent";
+
+const Signup = () => {
+  return (
+    <CenterComponent>
+      <div>
+        <h1>signup</h1>
+      </div>
+    </CenterComponent>
+  );
+};
+
+export default Signup;
