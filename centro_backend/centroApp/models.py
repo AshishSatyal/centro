50d58c8d9fb4b2fc7117b415012f02cca6b2f427
SshAayush <xayush.dc@gmail.com>
2024-07-07 20:14:00 +0545

    fix:User Model

--- centro_backend/centroApp/models.py
+++ centro_backend/centroApp/models.py
@@ -3,6 +3,41 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Create your models here.
+# class User(AbstractUser):
+#     name = models.CharField(max_length=255)
+#     email = models.CharField(max_length=255, unique=True)
+#     password = models.CharField(max_length=255)
+#     username = None
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = []
+
+from django.contrib.auth.models import BaseUserManager, AbstractUser
+from django.db import models
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
 class User(AbstractUser):
     name = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
@@ -11,3 +46,5 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = UserManager()
