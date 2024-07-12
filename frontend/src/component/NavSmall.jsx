8a9df2c6fe72003df99c430eda522ba7731cf857
AshishSatyal <ashishsatyal4@gmail.com>
2024-07-12 23:21:46 +0545

    Add:signup page

--- frontend/src/component/NavSmall.jsx
+++ frontend/src/component/NavSmall.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NavSmall = () => {
   return (
     <div className='flex justify-center items-center bg-black px-4 rounded-3xl w-full h-16'>
-      <div className='flex justify-between items-center gap-2 text-white'>
+      <div className='flex justify-around items-center gap-2 w-full text-white'>
         <h1>home</h1>
         <h1>anything</h1>
         <h1>Add product</h1>
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -9,38 +9,94 @@ const Signup = () => {
   return (
     <CenterComponent>
       <div className='flex flex-col justify-center items-center border w-full h-[100vh]'>
-        <h1 className='text-4xl capitalize font-bold'>signup</h1>
-        <div className='py-10 border'>
-          <form action=''>
-            <div className='flex gap-5 px-4'>
-              <div className='flex flex-col gap-2'>
-                <label className='' htmlFor='firstName'>
-                  First Name
+        <h1 className='font-bold text-4xl capitalize'>signup</h1>
+        <div className='py-5 border w-[25rem]'>
+          <form action='' className='w-full'>
+            <div className='flex flex-col gap-2'>
+              <div className='flex gap-2 px-4'>
+                <div className='flex flex-col gap-1'>
+                  <label className='font-semibold' htmlFor='firstName'>
+                    First Name
+                  </label>
+                  <input
+                    type='text'
+                    className='focus:border-gray-800 px-2 border border-black rounded-xl w-[11rem] h-10'
+                    placeholder='John'
+                    id='firstName'
+                    name='firstname'
+                  />
+                </div>
+                <div className='flex flex-col gap-1'>
+                  <label htmlFor='lastName' className='font-semibold'>
+                    Last Name
+                  </label>
+                  <input
+                    className='focus:border-gray-800 px-2 border border-black rounded-xl w-[11rem] h-10'
+                    type='text'
+                    placeholder='Doe'
+                    id='lastName'
+                    name='lastname'
+                  />
+                </div>
+              </div>
+              <div className='flex flex-col gap-1 px-4'>
+                <label htmlFor='email' className='font-semibold'>
+                  Email
                 </label>
                 <input
+                  className='focus:border-gray-800 px-2 border border-black rounded-xl w-[22rem] h-10'
                   type='text'
-                  className='focus:border-gray-800 px-4 border border-black rounded-xl w-[15rem] h-10'
-                  placeholder='John'
-                  id='firstName'
+                  placeholder='example@gmail.com'
+                  id='email'
+                  name='email'
                 />
               </div>
-              <div className='flex flex-col gap-2'>
-                <label htmlFor='lastName'>Last Name</label>
+              <div className='flex flex-col gap-1 px-4'>
+                <label htmlFor='number' className='font-semibold'>
+                  Number
+                </label>
                 <input
-                  className='focus:border-gray-800 px-4 border border-black rounded-xl w-[15rem] h-10'
-                  type='text'
-                  placeholder='Doe'
-                  id='lastName'
+                  className='focus:border-gray-800 px-2 border border-black rounded-xl w-[22rem] h-10 remove-arrow'
+                  type='number'
+                  placeholder='9812345678'
+                  id='number'
+                  name='number'
                 />
+              </div>
+              <div className='flex flex-col gap-1 px-4'>
+                <label htmlFor='password' className='font-semibold'>
+                  Password
+                </label>
+                <input
+                  className='focus:border-gray-800 px-2 border border-black rounded-xl w-[22rem] h-10'
+                  type='password'
+                  placeholder='********'
+                  id='password'
+                  name='password'
+                />
+              </div>
+              <div className='flex flex-col gap-1 px-4'>
+                <label htmlFor='confirm_password' className='font-semibold'>
+                  Confirm Password
+                </label>
+                <input
+                  className='focus:border-gray-800 px-2 border border-black rounded-xl w-[22rem] h-10'
+                  type='password'
+                  placeholder='********'
+                  id='confirm_password'
+                  name='confirm_password'
+                />
+              </div>
+              <div className='mt-5 px-4'>
+                <button
+                  className='border border-black hover:border-black/40 rounded-xl w-[22rem] h-10 font-semibold text-xl'
+                  onClick={handleForm}
+                  type='submit'
+                >
+                  Submit
+                </button>
               </div>
             </div>
-            <button
-              className='border border-black hover:border-black/40 rounded-xl w-40 h-10'
-              onClick={handleForm}
-              type='submit'
-            >
-              Submit
-            </button>
           </form>
         </div>
       </div>
