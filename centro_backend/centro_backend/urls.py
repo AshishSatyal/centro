6d63d3eca2ac42994a6045e9eec14548fa58fe6a
SshAayush <xayush.dc@gmail.com>
2024-08-13 22:56:18 +0545

    fix:Resolved impoer error

--- centro_backend/centro_backend/urls.py
+++ centro_backend/centro_backend/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 # from django.conf import settings
 from django.contrib import admin
-# from django.urls import path,include
+from django.urls import path,include
 from rest_framework.authtoken import views
 from django.conf import settings
 from django.conf.urls.static import static
